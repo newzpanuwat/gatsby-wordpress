41cae9decbc3301b27c0ccf05ba4a9bfd2119b9c
New Panuwat <pfcreatorit@gmail.com>
2019-01-21 02:08:05 +0700

    return favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Newhales.cc`,
-        short_name: `Newhales.cc`,
+        name: `Newhales`,
+        short_name: `Newhales`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -35,6 +35,39 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: 'Newhales', // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '2.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
     {
       resolve: `gatsby-source-wordpress`,
       options: {
