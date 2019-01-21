f67d28d6252993ee6a80a20da49f61c8af592742
New Panuwat <pfcreatorit@gmail.com>
2019-01-21 16:50:24 +0700

    add Listener

--- src/sw.js
+++ src/sw.js
@@ -8,6 +8,14 @@ workbox.precaching.precacheAndRoute([])
 
 
 // Another things
-self.addEventListener('push', () => {
-  
-})
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
